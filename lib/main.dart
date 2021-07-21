6baf3d72ab2de95f93a293e1082c24272ef264eb
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-07-21 13:05:41 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PersonalExpenses());
+
+class PersonalExpenses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
