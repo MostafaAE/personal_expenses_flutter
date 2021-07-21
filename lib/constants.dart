8c16fff8bdc49f40f949a5d5fcc4de2480f405c7
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-07-21 14:58:37 +0200

    transaction list ui

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kTxAmountStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 22,
+  color: Colors.purple,
+);
+
+const kTxTitleStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+
+const kTxDateStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  color: Colors.grey,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() => runApp(PersonalExpenses());
 
 class PersonalExpenses extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Expenses Planner',
+      home: HomePage(),
+    );
   }
 }
